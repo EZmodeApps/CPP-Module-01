ce61e86afc7a777d014e9fb587503e6126d9644d
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2022-03-30 04:22:29 +0300

    started ex04

---
+++ ex04/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main(int argc, char **argv)
+{
+	if (argc < 4) {
+		std::cerr << "Error: wrong number of arguments!" << std::endl;
+		return (1)
+	}
+	else if (argc == 4)
+	{
+		st
+	}
+	return 0;
+}
