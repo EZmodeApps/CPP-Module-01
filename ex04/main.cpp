9f87ed1d6316701bd41312e407b0fe9d64567cab
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2022-03-30 14:17:57 +0300

    started ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -2,15 +2,23 @@
 #include <string>
 #include <fstream>
 
-int main(int argc, char **argv)
+int main(void)//, char **argv)
 {
+	int argc = 4;
+	//void(argv);
 	if (argc < 4) {
 		std::cerr << "Error: wrong number of arguments!" << std::endl;
-		return (1)
+		return (1);
 	}
 	else if (argc == 4)
 	{
-		st
+		int a(3), b(4);
+		std::ifstream ifs("test");
+		ifs >> a >> b;
+		std::cout << ifs << std::endl;
+		std::ofstream ofs("test");
+		ofs << "3, 4" << std::endl;
+		ofs.close();
 	}
 	return 0;
 }
