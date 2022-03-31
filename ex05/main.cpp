ee420332b3353a27fa9800cb818856fead645494
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2022-03-31 17:40:43 +0300

    d01 done

---
+++ ex05/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.h"
+
+int main() {
+
+	Harl harl;
+
+	harl.complain("debug");
+	harl.complain("info");
+	harl.complain("warning");
+	harl.complain("error");
+	harl.complain("trash");
+}
